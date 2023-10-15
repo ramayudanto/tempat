d726e9acc1bc5c3b5d9fc5a661e221de9b4a1b0e
Rayhan Ramayudanto <99518246+ramayudanto@users.noreply.github.com>
2023-10-15 17:06:52 +0700

    GA Installation (#8)
    * test
    
    * login & sign up update
    
    * css fixes
    
    * GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { createContext, useState } from "react";
+import Script from "next/script";
 
 export const CoordinateContext = createContext(null as any);
 
@@ -13,6 +14,18 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
     <SessionProvider session={session}>
       <CoordinateContext.Provider value={{ latitude, longitude, setLatitude, setLongitude }}>
         <Component {...pageProps} />
+        <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID2}`}
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID2}');`
+          }
+        </Script>
       </CoordinateContext.Provider>
     </SessionProvider>
   );
