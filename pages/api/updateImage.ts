6db9aa7647534b3264ff8dfcacbc441350b72c8d
dikaaayy <andikayudhistira3@gmail.com>
2023-11-26 12:38:56 +0700

    update imag api fix

--- pages/api/updateImage.ts
+++ pages/api/updateImage.ts
@@ -40,6 +40,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         },
       },
     });
+    res.status(200).json({ success: true });
+    res.end();
   } catch (e) {
     console.log(e);
     res.status(500).json({ success: false, error: e });
