d50db9f15b2cbf9196f61eb7078f9381f45f9aa1
dikaaayy <andikayudhistira3@gmail.com>
2023-12-13 23:04:02 +0700

    search page fix

--- components/MainPage/RestaurantCard.tsx
+++ components/MainPage/RestaurantCard.tsx
@@ -7,7 +7,7 @@ import BookmarkButton from "./BookmarkButton";
 import { captureEvent } from "../../lib/posthog";
 
 export default function RestaurantCard({ restaurant }: any) {
-  const { featureImage, gofood_name, price_level: priceRange, OpeningHoursV2: opening_hours, categories: category, thumbnail, rating, routeName, userBookmark, user_ratings_total: totalRate } = restaurant;
+  const { featureImage, gofood_name, price_level: priceRange, opening_hours, OpeningHoursV2, categories: category, thumbnail, rating, routeName, userBookmark, user_ratings_total: totalRate } = restaurant;
   const session = false;
   // console.log(restaurant.ratingCount);
 
@@ -79,7 +79,11 @@ export default function RestaurantCard({ restaurant }: any) {
           </div>
 
           <p className=" text-darkGray text-opacity-70 text-xs">{translatePriceRange(priceRange)}</p>
-          <p className={`self-stretch text-xs not-italic font-normal leading-[normal] ${isRestaurantOpen(opening_hours).toLowerCase() === "buka" ? "text-green" : "text-[#952525]"}`}>{isRestaurantOpen(opening_hours)}</p>
+          {!opening_hours ? (
+            <p className={`self-stretch text-xs not-italic font-normal leading-[normal] ${isRestaurantOpen(OpeningHoursV2).toLowerCase() === "buka" ? "text-green" : "text-[#952525]"}`}>{isRestaurantOpen(OpeningHoursV2)}</p>
+          ) : (
+            <p className={`self-stretch text-xs not-italic font-normal leading-[normal] ${isRestaurantOpen(opening_hours).toLowerCase() === "buka" ? "text-green" : "text-[#952525]"}`}>{isRestaurantOpen(opening_hours)}</p>
+          )}
           {/* <p className="text-darkGray text-opacity-70 text-xs">{priceLogic(priceRange)}</p> */}
         </div>
       </a>
--- components/Search/SearchComponent.tsx
+++ components/Search/SearchComponent.tsx
@@ -12,18 +12,14 @@ import useDebounce from "../../lib/useDebounce";
 
 export default function Search({ fourCategories }: any) {
   const [recentSearch, setRecentSearch] = useState<any[]>([]);
-  const [recentSearchRestaurant, setRecentSearchRestaurant] = useState<any[]>(
-    []
-  );
+  const [recentSearchRestaurant, setRecentSearchRestaurant] = useState<any[]>([]);
   const searchRef = useRef<HTMLInputElement>(null);
   const [searchQuery, setSearchQuery] = useState<string>("");
   const router = useRouter();
   const debouncedText = useDebounce(searchQuery, 600);
   useEffect(() => {
     const query = JSON.parse(decryptLocalStorage("recentSearchQuery") || "[]");
-    const restaurant = JSON.parse(
-      decryptLocalStorage("recentSearchRestaurant") || "[]"
-    );
+    const restaurant = JSON.parse(decryptLocalStorage("recentSearchRestaurant") || "[]");
     setRecentSearch(query);
     setRecentSearchRestaurant(restaurant);
   }, []);
@@ -36,12 +32,8 @@ export default function Search({ fourCategories }: any) {
       recent.pop();
     }
     setRecentSearch(recent);
-    localStorage.setItem(
-      "recentSearchQuery",
-      encryptAES(JSON.stringify(recent))
-    );
+    localStorage.setItem("recentSearchQuery", encryptAES(JSON.stringify(recent)));
   };
-
   useEffect(() => {
     if (debouncedText === "") {
       router.push("/search", undefined, { shallow: true });
@@ -54,9 +46,7 @@ export default function Search({ fourCategories }: any) {
 
   return (
     <div className="w-screen transition-all pt-10 px-4 pb-24 overflow-x-hidden overflow-y-scroll h-screen mx-auto bg-white max-w-[420px]">
-      {!searchQuery && (
-        <p className="text-3xl font-semibold text-darkGray mb-6">Cari</p>
-      )}
+      {!searchQuery && <p className="text-3xl font-semibold text-darkGray mb-6">Cari</p>}
       <div className="flex gap-x-2  items-center mb-4 ">
         {searchQuery && (
           <button
@@ -65,19 +55,8 @@ export default function Search({ fourCategories }: any) {
               setSearchQuery("");
             }}
           >
-            <svg
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6 shrink-0"
-            >
-              <path
-                d="M19 12H5M5 12L12 19M5 12L12 5"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 shrink-0">
+              <path d="M19 12H5M5 12L12 19M5 12L12 5" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
           </button>
         )}
@@ -88,11 +67,7 @@ export default function Search({ fourCategories }: any) {
           {recentSearch.length !== 0 && (
             <>
               <p className="font-semibold text-sm mb-2">Pencarian terakhir</p>
-              <RecentSearchQuery
-                insert={insertRecentQuery}
-                setSearchQuery={setSearchQuery}
-                data={recentSearch}
-              />
+              <RecentSearchQuery insert={insertRecentQuery} setSearchQuery={setSearchQuery} data={recentSearch} />
             </>
           )}
           {recentSearchRestaurant.length !== 0 && (
